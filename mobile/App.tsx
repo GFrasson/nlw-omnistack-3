b068677855fe43d9cd761b6daaf6365ddaf66e7b
Gabriel Frasson <bielfc27@gmail.com>
2020-10-18 17:53:20 -0300

    Quinto dia finalizado

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import { useFonts } from "expo-font";
+import { 
+  Nunito_600SemiBold, 
+  Nunito_700Bold, 
+  Nunito_800ExtraBold 
+} from "@expo-google-fonts/nunito";
 
-import {useFonts} from 'expo-font';
-import {Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito';
+import Routes from "./src/routes";
 
-import Routes from './src/routes';
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
 
-export default function App() {
-    const [fontsLoaded] = useFonts({
-        Nunito_600SemiBold, 
-        Nunito_700Bold, 
-        Nunito_800ExtraBold
-    });
-
-    if (!fontsLoaded) {
-        return null;
-    }
-
-    return (
-        <Routes />
-    );
+  if (!fontsLoaded)  return null;
+  
+  return (
+    <Routes />
+  );
 }
+
+export default App;
